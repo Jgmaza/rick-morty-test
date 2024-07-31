996d272c61cc021075c4eef2980e30160097a83e
jgmaza <mazamendoza123@gmail.com>
2024-07-31 16:51:50 -0500

    chore: Add CORS headers to GET and POST requests

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -10,10 +10,36 @@ const server = new ApolloServer({
 
 const handler = startServerAndCreateNextHandler(server);
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*", // Cambia '*' a tu dominio específico en producción
+  "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function GET(request: Request) {
-  return handler(request);
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: corsHeaders,
+    });
+  }
+  const response = await handler(request);
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+  return response;
 }
 
 export async function POST(request: Request) {
-  return handler(request);
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: corsHeaders,
+    });
+  }
+  const response = await handler(request);
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+  return response;
 }
