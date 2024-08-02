21fb306c0f8c7f6c568736736f6c0c2fad81cf28
jgmaza <mazamendoza123@gmail.com>
2024-08-01 19:06:21 -0500

    chore: Update database connection configuration

--- src/lib/db/connection.ts
+++ src/lib/db/connection.ts
@@ -1,20 +1,16 @@
 import { Sequelize } from "sequelize";
+import pg from 'pg';
 
 // Conexión a la base de datos
 import dotenv from "dotenv";
+
 
 dotenv.config();
 
 console.log('DATABASE_URL:', process.env.DB_URL);
 
 const sequelize = new Sequelize(process.env.DB_URL!, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Solo si estás usando un certificado SSL autofirmado
-    },
-  },
+  dialectModule: pg
 });
 
 // Conection verification
